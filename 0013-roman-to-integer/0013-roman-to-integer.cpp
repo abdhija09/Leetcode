ad8cfd8512d50943948d1be0f40a4ced034fe1ad
Abdhija Nigam <abdhijanigam2609@gmail.com>
2026-03-29 13:07:49 +0530

    Time: 0 ms (100%), Space: 10.1 MB (86.28%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int romanToInt(string s) {
+        int count = 0;
+
+        for(int i = 0; i < s.size(); i++) {
+
+            if(s[i]=='I') {
+                if(i+1 < s.size() && (s[i+1]=='V' || s[i+1]=='X'))
+                    count -= 1;
+                else count += 1;
+            }
+
+            if(s[i]=='X') {
+                if(i+1 < s.size() && (s[i+1]=='L' || s[i+1]=='C'))
+                    count -= 10;
+                else count += 10;
+            }
+
+            if(s[i]=='C') {
+                if(i+1 < s.size() && (s[i+1]=='D' || s[i+1]=='M'))
+                    count -= 100;
+                else count += 100;
+            }
+
+            if(s[i]=='V') count += 5;
+            if(s[i]=='L') count += 50;
+            if(s[i]=='D') count += 500;
+            if(s[i]=='M') count += 1000;
+        }
+
+        return count;
+    }
+};
