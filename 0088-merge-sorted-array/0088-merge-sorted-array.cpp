2e836bea00b7ac3bcb9deca1bc425c85c77ec561
Abdhija Nigam <abdhijanigam2609@gmail.com>
2026-04-03 18:30:49 +0530

    Time: 0 ms (100%), Space: 12.4 MB (40.92%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -3,7 +3,9 @@ public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
         int r=nums1.size();
 
-       nums1.erase(nums1.end() -(r-m) , nums1.end());
+       for(int i=m;i<r;i++){
+        nums1.erase(nums1.begin()+i);
+       }
       
         for(int i=0;i<n;i++){
             nums1.push_back(nums2[i]);
