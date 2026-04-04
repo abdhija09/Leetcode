69cfe27505f2b4860ece1a0b060057fe1ff80ca0
Abdhija Nigam <abdhijanigam2609@gmail.com>
2026-04-04 16:57:41 +0530

    Time: 0 ms (100%), Space: 12.2 MB (15.44%) - LeetHub

--- 2367-number-of-arithmetic-triplets/2367-number-of-arithmetic-triplets.cpp
+++ 2367-number-of-arithmetic-triplets/2367-number-of-arithmetic-triplets.cpp
@@ -1,12 +1,17 @@
 class Solution {
 public:
     int arithmeticTriplets(vector<int>& nums, int diff) {
-        int n=nums.size();
-        int count=0;
-        for(int i=0;i<n;i++){
-            for(int j=i+1;j<n;j++){
-                for(int k=j+1;k<n;k++){
-                    if(nums[j]-nums[i]==nums[k]-nums[j] && nums[j]-nums[i]==diff ) count++;
+        int n = nums.size();
+        int count = 0;
+        set<int> hashset(nums.begin(), nums.end()); 
+
+        for(int i = 0; i < n; i++) {
+            for(int j = i+1; j < n; j++) {
+                if(nums[j]-nums[i]==diff) {        
+                    int th = nums[j]+diff;
+                    if(hashset.find(th)!=hashset.end()) { 
+                        count++;
+                    }
                 }
             }
         }
