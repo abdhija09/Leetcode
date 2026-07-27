7e18b75ca527e4634ebeaab661b718177b1b55b2
Abdhija Nigam <abdhijanigam2609@gmail.com>
2026-07-27 18:04:39 +0530

    Time: 0 ms (100%), Space: 18.6 MB (35.26%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.cpp
+++ 0101-symmetric-tree/0101-symmetric-tree.cpp
@@ -1,63 +1,13 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-    vector<vector<int> >rightSideView(TreeNode* root) {
-        queue<TreeNode*>q;
-        vector<vector<int>>ans;
-        if(root==NULL) return ans;
-        q.push(root);
- while(!q.empty()){
-            
-            int size=q.size();
-            vector<int>lvl;
-            bool anyReal=false;
-            for(int i=0;i<size;i++){
-                TreeNode* node=q.front();
-                q.pop();
-                if(node==NULL){
-                    lvl.push_back(INT_MIN);
-                    continue;
-                }
-                anyReal=true;
-                lvl.push_back(node->val);
-                q.push(node->left);
-                q.push(node->right);
-            }
-            if(!anyReal) break;
-            ans.push_back(lvl);
-        }
-     
-      return ans;
-    }
-    bool mirror(vector<int>&arr){
-        int n=arr.size();
-        int l=0,r=n-1;
-        bool ok=true;
-        while(l<=r){
-            if(arr[l]!=arr[r]) return false;
-            l++;
-            r--;
-        }
-        return ok;
+    bool isMirror(TreeNode* t1, TreeNode* t2){
+        if(t1==NULL && t2==NULL) return true;
+        if(t1==NULL || t2==NULL) return false;
+        if(t1->val != t2->val) return false;
+        return isMirror(t1->left, t2->right) && isMirror(t1->right, t2->left);
     }
     bool isSymmetric(TreeNode* root) {
-       vector<vector<int>>temp=rightSideView(root);
-       int m=temp.size();
-       bool b=true;
-       for(int i=0;i<m;i++){
-          if(!mirror(temp[i])) return false;
-       }
-       return b;
+        if(root==NULL) return true;
+        return isMirror(root->left, root->right);
     }
-
 };
