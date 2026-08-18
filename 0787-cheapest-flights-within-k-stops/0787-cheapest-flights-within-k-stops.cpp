200fd869d12d1223e38b9c1a86f1defeadc177c5
Abdhija Nigam <abdhijanigam2609@gmail.com>
2026-08-18 21:26:45 +0530

    Time: 3 ms (62.13%), Space: 18.1 MB (56.05%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
@@ -1,20 +1,34 @@
 class Solution {
 public:
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
-        vector<int> dist(n, INT_MAX);
-        dist[src] = 0;
+        vector<vector<pair<int,int>>> adj(n);
+        for (auto& f : flights) {
+            adj[f[0]].push_back({f[1], f[2]});
+        }
+        vector<vector<int>> dist(n, vector<int>(k+2, INT_MAX));
+        priority_queue<pair<int,pair<int,int>>, vector<pair<int,pair<int,int>>>, greater<pair<int,pair<int,int>>>> pq;
+        pq.push({0, {src, 0}});
+        dist[src][0] = 0;
 
-        for (int i = 0; i <= k; i++) {       
-            vector<int> temp = dist;             
-            for (auto& f : flights) {
-                int u = f[0], v = f[1], w = f[2];
-                if (dist[u] != INT_MAX && dist[u] + w < temp[v]) {
-                    temp[v] = dist[u] + w;
+        while (!pq.empty()) {
+            auto it = pq.top();
+            pq.pop();
+            int ds = it.first;
+            int d = it.second.first;
+            int stops = it.second.second;
+
+            if (d == dst) return ds;
+            if (stops > k) continue;         
+
+            for (auto& edge : adj[d]) {
+                int v = edge.first;
+                int w = edge.second;
+                if (ds + w < dist[v][stops+1]) {
+                    dist[v][stops+1] = ds + w;
+                    pq.push({ds + w, {v, stops+1}});
                 }
             }
-            dist = temp;
         }
-
-        return dist[dst] == INT_MAX ? -1 : dist[dst];
+        return -1;
     }
 };
